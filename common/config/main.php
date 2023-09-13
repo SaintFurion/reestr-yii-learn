c66be9f01fd441c766d08a8a13f8b76405f1dba9
SaintFurion <saintfurion56@gmail.com>
2023-09-13 14:16:24 +0500

    Развертывание проекта и установка dektrium/yii2-user

---
+++ common/config/main.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+    ],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset'
+    ],
+    'modules' => [
+        'user' => [
+            'class' => 'dektrium\user\Module',
+        ],
+    ],
+];
